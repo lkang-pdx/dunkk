c3ec6bfaf9ae4a5d82a0f51ccc9e981962a1089d
Luna Kang <luna.kang.ca@gmail.com>
2017-03-11 12:54:14 -0800

    Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://dunkk.herokuapp.com',
+                                               'http://dunkk.herokuapp.com']
+
+  config.action_cable.url = "wss://dunkk.herokuapp.com/cable"                                           
 end
